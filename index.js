94fa9d9a05192d1bfed54503f68f79f5813637e4
Jeff Fowler <jeffowler@gmail.com>
2023-01-30 12:48:41 +0100

    index and npmrc

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('postgres://postgres:postgres@127.0.0.1:5433/postgres');
+
+class Thing extends Model {}
+Thing.init({
+  name: DataTypes.STRING,
+}, { sequelize, modelName: 'things' });
+
+class ShardedThing extends Model {}
+ShardedThing.init({
+  name: DataTypes.STRING,
+}, { sequelize, modelName: 'sharded_things' });
+
+(async () => {
+  await sequelize.sync();
+
+  const thing = await Thing.create({
+    name: 'Bob Jackson',
+  });
+  console.log(thing.toJSON());
+
+  const shardedThing = await ShardedThing.create({
+    name: 'Sam Johnson',
+  });
+  console.log(shardedThing.toJSON());
+
+  await Thing.drop();
+  await ShardedThing.drop();
+})();
