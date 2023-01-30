2e53e96be867afc2b7b2d4ac3dc9f1182fccddae
Jeff Fowler <jeffowler@gmail.com>
2023-01-30 13:11:22 +0100

    connection in options

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(
-  "postgres://postgres:postgres@127.0.0.1:5433/main"
-);
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  database: 'main',
+  username: 'postgres',
+  password: 'postgres',
+  port: 5433
+});
 
 class Thing extends Model {}
 Thing.init(
