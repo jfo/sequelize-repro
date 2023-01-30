c11eaa0869b600bb05b9103255569f0f6a5b9c60
Jeff Fowler <jeffowler@gmail.com>
2023-01-30 13:01:44 +0100

    pretty

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:postgres@127.0.0.1:5433/main');
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(
+  "postgres://postgres:postgres@127.0.0.1:5433/main"
+);
 
 class Thing extends Model {}
-Thing.init({
-  name: DataTypes.STRING,
-}, { sequelize, modelName: 'things' });
+Thing.init(
+  {
+    name: DataTypes.STRING,
+  },
+  { sequelize, modelName: "things" }
+);
 
 class ShardedThing extends Model {}
-ShardedThing.init({
-  name: DataTypes.STRING,
-}, { sequelize, modelName: 'sharded_things' });
+ShardedThing.init(
+  {
+    name: DataTypes.STRING,
+  },
+  { sequelize, modelName: "sharded_things" }
+);
 
 (async () => {
   await sequelize.sync();
-
-  const thing = await Thing.create({
-    name: 'Bob Jackson',
-  });
-  console.log(thing.toJSON());
-
-  const shardedThing = await ShardedThing.create({
-    name: 'Sam Johnson',
-  });
-  console.log(shardedThing.toJSON());
 })();
